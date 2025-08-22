16d3a13cc8726519976a0e7fe0120e4bb1798266
cyxyucheng <cyxyucheng@tencent.com>
2025-08-22 15:57:46 +0800

    Add PluginManager and SearchDialog classes to handle plugin management and search functionality in the application.

---
+++ include/search_dialog.h
@@ -0,0 +1,51 @@
+#ifndef SEARCH_DIALOG_H
+#define SEARCH_DIALOG_H
+
+#include <QDialog>
+#include <QLineEdit>
+#include <QPushButton>
+#include <QCheckBox>
+#include <QRadioButton>
+#include <QVBoxLayout>
+#include <QHBoxLayout>
+#include <QLabel>
+#include <QGroupBox>
+
+class SearchDialog : public QDialog {
+    Q_OBJECT
+
+public:
+    explicit SearchDialog(QWidget *parent = nullptr);
+    
+    QString getSearchText() const;
+    QString getReplaceText() const;
+    bool isCaseSensitive() const;
+    bool isWholeWord() const;
+    bool isRegex() const;
+    bool searchInTree() const;
+    bool searchInEditor() const;
+    bool isReplaceMode() const;
+
+private slots:
+    void onSearchModeChanged();
+    void onReplaceModeChanged();
+
+private:
+    void setupUi();
+    
+    QLineEdit* searchEdit_;
+    QLineEdit* replaceEdit_;
+    QPushButton* findButton_;
+    QPushButton* replaceButton_;
+    QPushButton* replaceAllButton_;
+    QCheckBox* caseSensitiveCheck_;
+    QCheckBox* wholeWordCheck_;
+    QCheckBox* regexCheck_;
+    QCheckBox* searchTreeCheck_;
+    QCheckBox* searchEditorCheck_;
+    QRadioButton* searchModeRadio_;
+    QRadioButton* replaceModeRadio_;
+    QGroupBox* replaceGroup_;
+};
+
+#endif // SEARCH_DIALOG_H 
