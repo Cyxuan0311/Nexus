1759f865722acb9c0c965325aabf704fbf529c8f
cyxyucheng <cyxyucheng@tencent.com>
2025-08-22 15:57:39 +0800

    Add code folding functionality with new CodeFoldingArea and FoldingTextEdit classes, and integrate them into the main window for XML editing.

---
+++ include/code_folding.h
@@ -0,0 +1,62 @@
+#ifndef CODE_FOLDING_H
+#define CODE_FOLDING_H
+
+#include <QPlainTextEdit>
+#include <QTextBlock>
+#include <QPainter>
+#include <QMouseEvent>
+#include <QSet>
+#include <QWidget>
+
+class FoldingTextEdit;
+
+class CodeFoldingArea : public QWidget {
+    Q_OBJECT
+
+public:
+    explicit CodeFoldingArea(FoldingTextEdit* editor);
+    
+    void foldAll();
+    void unfoldAll();
+
+protected:
+    void paintEvent(QPaintEvent* event) override;
+    void mousePressEvent(QMouseEvent* event) override;
+    void mouseMoveEvent(QMouseEvent* event) override;
+
+private slots:
+    void updateFoldableLines();
+
+private:
+    FoldingTextEdit* editor_;
+    QSet<int> foldableLines_;
+    QSet<int> foldedBlocks_;
+    
+    // Helper methods
+    int getBlockNumberAtY(int y) const;
+    bool isFoldableXmlTag(const QString& text) const;
+    void foldBlock(int blockNumber);
+    void unfoldBlock(int blockNumber);
+    QString extractTagName(const QString& text) const;
+    QTextBlock findClosingTag(const QTextBlock& startBlock, const QString& tagName) const;
+    bool isBlockInFold(int blockNumber) const;
+};
+
+class FoldingTextEdit : public QPlainTextEdit {
+    Q_OBJECT
+
+public:
+    explicit FoldingTextEdit(QWidget* parent = nullptr);
+    
+    void foldAll();
+    void unfoldAll();
+    CodeFoldingArea* getFoldingArea() const;
+
+protected:
+    void resizeEvent(QResizeEvent* event) override;
+
+private:
+    CodeFoldingArea* foldingArea_;
+};
+
+#endif // CODE_FOLDING_H 
--- include/main_window.h
+++ include/main_window.h
@@ -5,6 +5,7 @@
 #include <QSplitter>
 #include <QTreeWidget>
 #include <QTextEdit>
+#include <QPlainTextEdit>
 #include <QPushButton>
 #include <QFileDialog>
 #include <QLabel>
@@ -18,7 +19,15 @@
 #include <QMessageBox>
 #include <QApplication>
 #include <QStyle>
+#include <QSyntaxHighlighter>
+#include <QTextCharFormat>
+#include <QFont>
+#include <QRegExp>
 #include "xml_parser.h"
+#include "xml_serializer.h"
+#include "xml_highlighter.h"
+#include "search_dialog.h"
+#include "code_folding.h"
 
 class MainWindow : public QMainWindow {
     Q_OBJECT
@@ -31,6 +40,18 @@ private slots:
     void openFile();
     void parseXml();
     void saveFile();
+    void exportToJson();
+    void exportToYaml();
+    void exportToCsv();
+    void importFromJson();
+    void importFromYaml();
+    void toggleEditMode();
+    void saveXmlContent();
+    void showSearchDialog();
+    void performSearch();
+    void performReplace();
+    void foldAllXml();
+    void unfoldAllXml();
     void about();
     void onTreeItemClicked(QTreeWidgetItem* item, int column);
 
@@ -48,21 +69,39 @@ private:
     QSplitter* mainSplitter_;
     QWidget* leftPanel_;
     QWidget* rightPanel_;
+    QWidget* centerPanel_;
     QTreeWidget* treeWidget_;
     QTextEdit* detailsTextEdit_;
+    FoldingTextEdit* xmlEditor_;
     QPushButton* parseButton_;
     QPushButton* openButton_;
+    QPushButton* editButton_;
+    QPushButton* saveButton_;
     QLabel* fileLabel_;
     
     // Data
     XmlParser parser_;
+    XmlSerializer serializer_;
     std::shared_ptr<XmlNode> rootNode_;
     std::string currentFilePath_;
+    bool isEditing_;
+    QString originalXmlContent_;
+    SearchDialog* searchDialog_;
+    QList<QTreeWidgetItem*> searchResults_;
+    int currentSearchIndex_;
     
     // Actions
     QAction* openAction_;
     QAction* saveAction_;
     QAction* parseAction_;
+    QAction* exportJsonAction_;
+    QAction* exportYamlAction_;
+    QAction* exportCsvAction_;
+    QAction* importJsonAction_;
+    QAction* importYamlAction_;
+    QAction* searchAction_;
+    QAction* foldAllAction_;
+    QAction* unfoldAllAction_;
     QAction* exitAction_;
     QAction* aboutAction_;
 };
