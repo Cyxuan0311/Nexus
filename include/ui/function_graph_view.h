1201fe944b809aebdd999c75c55bca69967e1702
cyxyucheng <cyxyucheng@tencent.com>
2025-09-01 16:14:08 +0800

    Add FunctionGraphView class and related components for visualizing C++ function relationships. This includes FunctionNode and FunctionEdge classes for graph representation, along with UI elements for interaction and layout management, enhancing the analysis of C++ code structure.

---
+++ include/ui/function_graph_view.h
@@ -0,0 +1,147 @@
+#ifndef FUNCTION_GRAPH_VIEW_H
+#define FUNCTION_GRAPH_VIEW_H
+
+#include <QWidget>
+#include <QGraphicsView>
+#include <QGraphicsScene>
+#include <QGraphicsItem>
+#include <QGraphicsEllipseItem>
+#include <QGraphicsTextItem>
+#include <QGraphicsLineItem>
+#include <QGraphicsArrowItem>
+#include <QPen>
+#include <QBrush>
+#include <QVBoxLayout>
+#include <QHBoxLayout>
+#include <QPushButton>
+#include <QLabel>
+#include <QScrollArea>
+#include <QComboBox>
+#include <map>
+#include <vector>
+#include "cpp_parser.h"
+
+class FunctionNode;
+class FunctionEdge;
+
+class FunctionGraphView : public QWidget {
+    Q_OBJECT
+
+public:
+    explicit FunctionGraphView(QWidget* parent = nullptr);
+    ~FunctionGraphView();
+    
+    // 设置 C++ 解析数据
+    void setParserData(const CppParser& parser);
+    
+    // 生成函数关系图
+    void generateGraph();
+    
+    // 清空图形
+    void clearGraph();
+
+public slots:
+    void zoomIn();
+    void zoomOut();
+    void resetZoom();
+    void autoLayout();
+    void showFunctionDetails(const QString& functionName);
+
+private slots:
+    void onNodeClicked(FunctionNode* node);
+    void onLayoutTypeChanged(int index);
+
+private:
+    void setupUI();
+    void createNodes();
+    void createEdges();
+    void layoutNodes();
+    void layoutCircular();
+    void layoutHierarchical();
+    void layoutForceDirected();
+    
+    // UI 组件
+    QVBoxLayout* mainLayout_;
+    QHBoxLayout* controlsLayout_;
+    QGraphicsView* graphView_;
+    QGraphicsScene* scene_;
+    QPushButton* zoomInButton_;
+    QPushButton* zoomOutButton_;
+    QPushButton* resetZoomButton_;
+    QPushButton* autoLayoutButton_;
+    QComboBox* layoutCombo_;
+    QLabel* infoLabel_;
+    QScrollArea* detailsArea_;
+    QLabel* detailsLabel_;
+    
+    // 数据
+    std::vector<CppFunction> functions_;
+    std::vector<CppClass> classes_;
+    std::map<std::string, std::vector<std::string>> functionCalls_;
+    
+    // 图形节点
+    std::map<std::string, FunctionNode*> nodes_;
+    std::vector<FunctionEdge*> edges_;
+    
+    // 当前选中的节点
+    FunctionNode* selectedNode_;
+    
+    // 布局参数
+    int nodeRadius_;
+    int nodeSpacing_;
+    int levelSpacing_;
+};
+
+// 函数节点类
+class FunctionNode : public QObject, public QGraphicsEllipseItem {
+    Q_OBJECT
+    
+public:
+    FunctionNode(const CppFunction& function, QGraphicsItem* parent = nullptr);
+    
+    const CppFunction& getFunction() const { return function_; }
+    void setSelected(bool selected);
+    void updatePosition(const QPointF& pos);
+    
+    // 高亮相关函数
+    void highlightCalledFunctions(bool highlight);
+    void highlightCallingFunctions(bool highlight);
+    
+protected:
+    void mousePressEvent(QGraphicsSceneMouseEvent* event) override;
+    void mouseReleaseEvent(QGraphicsSceneMouseEvent* event) override;
+    void paint(QPainter* painter, const QStyleOptionGraphicsItem* option, QWidget* widget) override;
+
+signals:
+    void nodeClicked(FunctionNode* node);
+
+private:
+    CppFunction function_;
+    QGraphicsTextItem* textItem_;
+    bool isSelected_;
+    bool isHighlighted_;
+};
+
+// 函数调用边类
+class FunctionEdge : public QGraphicsLineItem {
+public:
+    FunctionEdge(FunctionNode* from, FunctionNode* to, QGraphicsItem* parent = nullptr);
+    
+    void updateLine();
+    void setHighlighted(bool highlighted);
+    
+    FunctionNode* fromNode_;
+    FunctionNode* toNode_;
+    
+protected:
+    void paint(QPainter* painter, const QStyleOptionGraphicsItem* option, QWidget* widget) override;
+
+private:
+    bool isHighlighted_;
+    QGraphicsPolygonItem* arrowHead_;
+    
+    void createArrowHead();
+    QPolygonF createArrowPolygon(const QPointF& tip, const QPointF& tail);
+};
+
+#endif // FUNCTION_GRAPH_VIEW_H 
--- include/ui/main_window.h
+++ include/ui/main_window.h
@@ -23,87 +23,107 @@
 #include <QTextCharFormat>
 #include <QFont>
 #include <QRegExp>
+#include <QTabWidget>
+#include <QTextBrowser>
 #include "xml_parser.h"
 #include "xml_serializer.h"
 #include "xml_highlighter.h"
+#include "cpp_highlighter.h"
+#include "cpp_parser.h"
+#include "function_graph_view.h"
 #include "search_dialog.h"
 #include "code_folding.h"
 
 class MainWindow : public QMainWindow {
-    Q_OBJECT
+	Q_OBJECT
 
 public:
-    MainWindow(QWidget *parent = nullptr);
-    ~MainWindow();
+	MainWindow(QWidget *parent = nullptr);
+	~MainWindow();
 
 private slots:
-    void openFile();
-    void parseXml();
-    void saveFile();
-    void exportToJson();
-    void exportToYaml();
-    void exportToCsv();
-    void importFromJson();
-    void importFromYaml();
-    void toggleEditMode();
-    void saveXmlContent();
-    void showSearchDialog();
-    void performSearch();
-    void performReplace();
-    void foldAllXml();
-    void unfoldAllXml();
-    void about();
-    void onTreeItemClicked(QTreeWidgetItem* item, int column);
+	void openFile();
+	void parseXml();
+	void parseCpp();
+	void generateFunctionGraph();
+	void saveFile();
+	void exportToJson();
+	void exportToYaml();
+	void exportToCsv();
+	void importFromJson();
+	void importFromYaml();
+	void toggleEditMode();
+	void saveXmlContent();
+	void showSearchDialog();
+	void performSearch();
+	void performReplace();
+	void foldAllXml();
+	void unfoldAllXml();
+	void about();
+	void onTreeItemClicked(QTreeWidgetItem* item, int column);
 
 private:
-    void setupUi();
-    void setupMenuBar();
-    void setupToolBar();
-    void setupStatusBar();
-    void setupStyle();
-    void populateTreeWidget(const std::shared_ptr<XmlNode>& node, QTreeWidgetItem* parentItem = nullptr);
-    void displayNodeDetails(const std::shared_ptr<XmlNode>& node);
-    void clearDisplay();
-    
-    // UI Components
-    QSplitter* mainSplitter_;
-    QWidget* leftPanel_;
-    QWidget* rightPanel_;
-    QWidget* centerPanel_;
-    QTreeWidget* treeWidget_;
-    QTextEdit* detailsTextEdit_;
-    FoldingTextEdit* xmlEditor_;
-    QPushButton* parseButton_;
-    QPushButton* openButton_;
-    QPushButton* editButton_;
-    QPushButton* saveButton_;
-    QLabel* fileLabel_;
-    
-    // Data
-    XmlParser parser_;
-    XmlSerializer serializer_;
-    std::shared_ptr<XmlNode> rootNode_;
-    std::string currentFilePath_;
-    bool isEditing_;
-    QString originalXmlContent_;
-    SearchDialog* searchDialog_;
-    QList<QTreeWidgetItem*> searchResults_;
-    int currentSearchIndex_;
-    
-    // Actions
-    QAction* openAction_;
-    QAction* saveAction_;
-    QAction* parseAction_;
-    QAction* exportJsonAction_;
-    QAction* exportYamlAction_;
-    QAction* exportCsvAction_;
-    QAction* importJsonAction_;
-    QAction* importYamlAction_;
-    QAction* searchAction_;
-    QAction* foldAllAction_;
-    QAction* unfoldAllAction_;
-    QAction* exitAction_;
-    QAction* aboutAction_;
+	void setupUi();
+	void setupMenuBar();
+	void setupToolBar();
+	void setupStatusBar();
+	void setupStyle();
+	void populateTreeWidget(const std::shared_ptr<XmlNode>& node, QTreeWidgetItem* parentItem = nullptr);
+	void displayNodeDetails(const std::shared_ptr<XmlNode>& node);
+	void clearDisplay();
+	void renderMarkdownPreview();
+	void applyHighlighterForCurrentFile();
+	bool isCurrentFileMarkdown() const;
+	bool isCurrentFileCpp() const;
+	
+	// UI Components
+	QSplitter* mainSplitter_;
+	QWidget* leftPanel_;
+	QWidget* rightPanel_;
+	QWidget* centerPanel_;
+	QTreeWidget* treeWidget_;
+	QTextEdit* detailsTextEdit_;
+	FoldingTextEdit* xmlEditor_;
+	QPushButton* parseButton_;
+	QPushButton* cppParseButton_;
+	QPushButton* graphButton_;
+	QPushButton* openButton_;
+	QPushButton* editButton_;
+	QPushButton* saveButton_;
+	QLabel* fileLabel_;
+	QTabWidget* rightTabs_;
+	QTextBrowser* markdownPreview_;
+	FunctionGraphView* functionGraphView_;
+	
+	// Data
+	XmlParser parser_;
+	XmlSerializer serializer_;
+	CppParser cppParser_;
+	std::shared_ptr<XmlNode> rootNode_;
+	std::string currentFilePath_;
+	bool isEditing_;
+	QString originalXmlContent_;
+	SearchDialog* searchDialog_;
+	QList<QTreeWidgetItem*> searchResults_;
+	int currentSearchIndex_;
+	bool isMarkdownMode_;
+	bool isCppMode_;
+	QSyntaxHighlighter* currentHighlighter_;
+	
+	// Actions
+	QAction* openAction_;
+	QAction* saveAction_;
+	QAction* parseAction_;
+	QAction* exportJsonAction_;
+	QAction* exportYamlAction_;
+	QAction* exportCsvAction_;
+	QAction* importJsonAction_;
+	QAction* importYamlAction_;
+	QAction* searchAction_;
+	QAction* foldAllAction_;
+	QAction* unfoldAllAction_;
+	QAction* exitAction_;
+	QAction* aboutAction_;
 };
 
 #endif // MAIN_WINDOW_H 
